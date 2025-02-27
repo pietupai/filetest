1768091d7de514b74e95f17c89266701e6351ead
pietupai <pietupai@gmail.com>
2025-02-27 13:52:30 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,35 +3,52 @@ import fs from 'fs';
 import path from 'path';
 
 const server = http.createServer(async (req, res) => {
-  if (req.method === 'GET' && req.url === '/api/index.js') {
-    let filePath = path.join(process.cwd(), 'koe.txt');
+  if (req.url === '/public/test-client.html') {
+    // Serve the static HTML file
+    let filePath = path.join(process.cwd(), 'public', 'test-client.html');
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Virhe tiedostoa luettaessa');
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
       }
     });
-  } else if (req.method === 'POST' && req.url === '/api/index.js') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      let contentToWrite = JSON.parse(body).content;
+  } else if (req.url.startsWith('/api/index.js')) {
+    if (req.method === 'GET') {
       let filePath = path.join(process.cwd(), 'koe.txt');
-      fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Virhe tiedostoon kirjoittamisessa');
+          res.end('Virhe tiedostoa luettaessa');
         } else {
           res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Tiedostoon kirjoittaminen onnistui!');
+          res.end(data);
         }
       });
-    });
+    } else if (req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        let contentToWrite = JSON.parse(body).content;
+        let filePath = path.join(process.cwd(), 'koe.txt');
+        fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
+          if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Virhe tiedostoon kirjoittamisessa');
+          } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Tiedostoon kirjoittaminen onnistui!');
+          }
+        });
+      });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Reittiä ei löydy');
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Reittiä ei löydy');
